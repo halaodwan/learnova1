6897fcd519cd3f58c0b243fcfdc7306016a9be41
Hala Rabah Madini <262595024+halamadini@users.noreply.github.com>
2026-04-17 21:11:24 +0300

    fix and update

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const studyMaterialRoutes = require('./Routes/StudyMaterialRoutes');
 
 const contentRoutes = require('./Routes/ContentRoutes');
 
-const flashcardRoutes = require('./Routes/FlachsardRoutes');
+const flashcardRoutes = require('./Routes/FlashcardRoutes');
 
 const examRoutes = require('./Routes/ExamRoutes');
 
@@ -36,7 +36,7 @@ app.use('/study-materials', studyMaterialRoutes);
 
 app.use('/contents', contentRoutes);
 
-app.use('/flashcards', flashsardRoutes);
+app.use('/flashcards', flashcardRoutes);
 
 app.use('/exams', examRoutes);
 
