6a11d4a713aeb883b42c226d90db830948985a3b
Hala Rabah Madini <262595024+halamadini@users.noreply.github.com>
2026-05-09 09:46:22 +0300

    update work

--- models/option.js
+++ models/option.js
@@ -4,11 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Option extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+   
     static associate(models) {
       // define association here
     Option.belongsTo(models.Question, {
